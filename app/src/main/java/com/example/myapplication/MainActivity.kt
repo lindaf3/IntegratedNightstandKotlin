241769a86238c6ed9aa07063d2c840d496997d1a
Linda Fan <linda.fan.4556@gmail.com>
2022-10-24 13:07:56 -0700

    material time picker

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,42 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val alarmStart: Button = findViewById(R.id.alarmStart)
+        val alarmEnd: Button = findViewById(R.id.alarmEnd)
+        val alarmSet: Button = findViewById(R.id.alarmSetter)
+
+        alarmStart.setOnClickListener{
+            val startTimePicker: MaterialTimePicker = MaterialTimePicker.Builder()
+                .setTitleText("Select Alarm Start")
+                .setHour(12)
+                .setMinute(10)
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .build()
+            startTimePicker.show(supportFragmentManager, "MainActivity")
+        }
+        alarmEnd.setOnClickListener{
+            val endTimePicker: MaterialTimePicker = MaterialTimePicker.Builder()
+                .setTitleText("Select Alarm End")
+                .setHour(12)
+                .setMinute(10)
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .build()
+            endTimePicker.show(supportFragmentManager, "MainActivity")
+        }
+
 
 
 
     }
 }
+
+
