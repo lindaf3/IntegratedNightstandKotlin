4b91c8df2a707acb4085d5b0391d61f0c59c82ae
Linda Fan <linda.fan.4556@gmail.com>
2023-01-04 14:48:40 -0800

    shared pref new solution: note- refresh is necessary for change

--- app/src/main/java/com/example/myapplication/ViewDataFragment.kt
+++ app/src/main/java/com/example/myapplication/ViewDataFragment.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import CloudClient
+import android.content.Context
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -29,49 +30,55 @@ class ViewDataFragment : Fragment() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        setFragmentResultListener("requestKey") { _, bundle ->
-            val startHour = bundle.getInt("start hour")
-            val endHour = bundle.getInt("end hour")
-            val startMinute = bundle.getInt("start minute")
-            val endMinute = bundle.getInt("end minute")
-            var domLow = startHour +startMinute/60.0
-            var domHigh = endHour + endMinute/60.0
+        val times = context?.getSharedPreferences("times", Context.MODE_PRIVATE)
+        var startHour : Int = times?.getInt("start hour", -1) ?: -1
+        var startMinute: Int = times?.getInt("start minute", -1) ?: -1
+        var endHour: Int = times?.getInt("end hour", -1) ?: -1
+        var endMinute :Int = times?.getInt("end minute", -1) ?: -1
+        var domBounds = getDomBounds(startHour, startMinute, endHour, endMinute)
 
-            super.onViewCreated(view, savedInstanceState)
-            var sleepDataX = mutableListOf<Double>()
-            var sleepData = mutableListOf<Double>()
-            val dataSeries: XYSeries = SimpleXYSeries(sleepDataX.toList(), sleepData.toList(), "Sleep Data" )
-            val dataFormat = FastLineAndPointRenderer.Formatter(Color.BLUE, Color.BLACK, null, null)
+        super.onViewCreated(view, savedInstanceState)
+        var sleepDataX = mutableListOf<Double>()
+        var sleepData = mutableListOf<Double>()
+        val dataSeries: XYSeries = SimpleXYSeries(sleepDataX.toList(), sleepData.toList(), "Sleep Data" )
+        val dataFormat = FastLineAndPointRenderer.Formatter(Color.BLUE, Color.BLACK, null, null)
 
-            val graph = view.findViewById<XYPlot>(R.id.graph)
-            val refresh = view.findViewById<Button>(R.id.refresh)
-            graph.addSeries(dataSeries, dataFormat)
+        val graph = view.findViewById<XYPlot>(R.id.graph)
+        val refresh = view.findViewById<Button>(R.id.refresh)
+        graph.addSeries(dataSeries, dataFormat)
 
-            if(startHour == -1 ||startMinute == -1 || endHour == -1 || endMinute == -1){
-                domHigh = 24.0
-                domLow = 0.0
+        graph.setDomainBoundaries(domBounds.first, domBounds.second, BoundaryMode.FIXED)
+        graph.setRangeBoundaries(0, 100, BoundaryMode.FIXED)
+
+        refresh.setOnClickListener{
+            graph.clear()
+            val updatedData = updateGraph()
+            for ( (time,amplitude) in updatedData) {
+                sleepDataX.add(time)
+                sleepData.add(amplitude)
             }
-            graph.setDomainBoundaries(domLow, domHigh, BoundaryMode.FIXED)
-            graph.setRangeBoundaries(0, 100, BoundaryMode.FIXED)
+            val updatedDataSeries: XYSeries = SimpleXYSeries(sleepDataX.toList(), sleepData.toList(), "Sleep Data" )
+            graph.addSeries(updatedDataSeries, dataFormat)
+            startHour = times?.getInt("start hour", -1) ?: -1
+            startMinute = times?.getInt("start minute", -1) ?: -1
+            endHour = times?.getInt("end hour", -1) ?: -1
+            endMinute = times?.getInt("end minute", -1) ?: -1
+            domBounds = getDomBounds(startHour, startMinute, endHour, endMinute)
+            graph.setDomainBoundaries(domBounds.first, domBounds.second, BoundaryMode.FIXED)
+            graph.redraw()
 
-
-
-            refresh.setOnClickListener{
-                graph.clear()
-                val updatedData = updateGraph()
-                for ( (time,amplitude) in updatedData) {
-                    sleepDataX.add(time)
-                    sleepData.add(amplitude)
-                }
-                val updatedDataSeries: XYSeries = SimpleXYSeries(sleepDataX.toList(), sleepData.toList(), "Sleep Data" )
-                graph.addSeries(updatedDataSeries, dataFormat)
-                graph.redraw()
-
-                Toast.makeText(requireActivity().application, "Updated Data!", Toast.LENGTH_SHORT).show()
-            }
-
+            Toast.makeText(requireActivity().application, "Updated Data!", Toast.LENGTH_SHORT).show()
         }
-
+    }
+    private fun getDomBounds(startHour: Int, startMinute: Int, endHour: Int, endMinute: Int): Pair<Double, Double>
+    {
+        var domLow = startHour +startMinute/60.0
+        var domHigh = endHour + endMinute/60.0
+        if(startHour == -1 ||startMinute == -1 || endHour == -1 || endMinute == -1){
+            domHigh = 24.0
+            domLow = 0.0
+        }
+        return Pair(domLow, domHigh)
 
     }
     @RequiresApi(Build.VERSION_CODES.O)
